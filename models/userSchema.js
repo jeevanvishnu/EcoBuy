e312f19b8377c116a342c004f4bb04444d7fb825
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-02-14 01:20:22 +0530

    add login page

--- models/userSchema.js
+++ models/userSchema.js
@@ -11,7 +11,7 @@ const userSchema = new Schema ({
         required:true,
         unique:true
     },
-    phone:{
+    mobile:{
         type:String,
         required:false,
         unique: false,
