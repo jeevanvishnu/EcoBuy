e6484ea6975477e6a5bfaa4a46e59bb1715d8b1e
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-02-15 22:27:08 +0530

    feat:Add customer block and unblock

--- router/adminRouter/adminRouter.js
+++ router/adminRouter/adminRouter.js
@@ -2,6 +2,7 @@ import express from "express"
 const router =express.Router()
 import adminController from "../../controllers/admin/adminController.js"
 import auth from '../../middlewares/auth.js'
+import customerController from '../../controllers/admin/customerController.js'
 const {userAuth , adminAuth} = auth
 
 router.get('/pageerror',adminController.pageerror)
@@ -9,6 +10,12 @@ router.get('/login',adminController.loadLogin)
 router.post('/login',adminController.login)
 router.get('/',adminAuth,adminController.loadDashboard)
 router.get('/logout',adminController.logout)
+
+// Cumstomer Managment 
+router.get('/Users',adminAuth,customerController.customerInfo)
+router.get('/blockCustomer',customerController.customerBlocked)
+router.get('/unblockCustomer',customerController.customerUnBlocked)
+
 
 
 
