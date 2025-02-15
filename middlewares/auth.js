a450e07a1a633e5942d3e86299cc46dc950d06f2
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-02-15 13:31:29 +0530

    fix:Some logout issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-import User from "../models/userSchema";
+import User from "../models/userSchema.js";
 
 
 
--- router/adminRouter/adminRouter.js
+++ router/adminRouter/adminRouter.js
@@ -1,12 +1,14 @@
 import express from "express"
 const router =express.Router()
 import adminController from "../../controllers/admin/adminController.js"
-import {userAuth,adminAuth} from '../../middlewares/auth.js'
+import auth from '../../middlewares/auth.js'
+const {userAuth , adminAuth} = auth
 
+router.get('/pageerror',adminController.pageerror)
 router.get('/login',adminController.loadLogin)
 router.post('/login',adminController.login)
 router.get('/',adminAuth,adminController.loadDashboard)
-
+router.get('/logout',adminController.logout)
 
 
 
